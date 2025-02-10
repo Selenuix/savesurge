98acde3407231ec434fa4416efe01d9826b3c219
Selenuix <anthony.ciszek@outlook.com>
2025-02-10 23:44:29 +0100

    feat(landing): extract constants into dedicated files

--- src/components/landing/features.tsx
+++ src/components/landing/features.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import {motion} from "framer-motion";
-import {features} from "@/consts";
+import {features} from "@/consts/landing-features";
 
 export const Features = () => {
   return (<section className="py-24 bg-white px-6" id="features-section">
--- src/consts/landing-features.ts
+++ src/consts/landing-features.ts
@@ -1,16 +1,5 @@
-import {
-  BanknoteIcon,
-  BellIcon, BellRing,
-  Home,
-  LineChart,
-  PieChart,
-  SearchIcon,
-  Shield,
-  TagsIcon,
-  Wallet,
-  Wallet2,
-} from "lucide-react";
-import {Feature, MenuItem} from "@/types/types";
+import {Feature} from "@/types/types";
+import {BanknoteIcon, BellIcon, LineChart, PieChart, SearchIcon, Shield, TagsIcon, Wallet} from "lucide-react";
 
 export const features: Feature[] = [{
   icon: Wallet,
@@ -45,10 +34,3 @@ export const features: Feature[] = [{
   title: "Bank-Level Security",
   description: "Rest easy knowing your data is protected with enterprise-grade encryption and secure authentication protocols."
 }];
-
-export const menuItems: MenuItem[] = [
-  {icon: Home, label: "Overview", path: "/dashboard"},
-  {icon: Wallet2, label: "Transactions", path: "/dashboard/transactions"},
-  {icon: PieChart, label: "Budget", path: "/dashboard/budget"},
-  {icon: BellRing, label: "Alerts", path: "/dashboard/alerts"},
-]
